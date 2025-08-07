e4b6970707742f4fd41c8ac071bed144d3146675
debasishs-kr <debashish.singh@kroger.com>
2025-08-07 23:15:09 +0530

    added multi browser capability

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -3,22 +3,29 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.util.Map;
+import java.util.Properties;
 
 public class DriverFactory {
     private static WebDriver driver;
 
     public static WebDriver initDriver() {
         if (driver == null) {
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--disable-notifications", "--incognito");
-            options.setExperimentalOption("prefs", Map.of(
-                    "credentials_enable_service", false,
-                    "profile.password_manager_enabled", false
-            ));
+            Properties prop = ConfigReader.initProperties();
+            String browser = prop.getProperty("browser");
 
-            driver = new ChromeDriver(options);
+            if (browser.equalsIgnoreCase("chrome")) {
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--disable-notifications", "--incognito");
+                options.setExperimentalOption("prefs", Map.of(
+                        "credentials_enable_service", false,
+                        "profile.password_manager_enabled", false
+                ));
+                driver = new ChromeDriver(options);
+            } else {
+                throw new RuntimeException("Unsupported browser: " + browser);
+            }
+
             driver.manage().window().maximize();
         }
         return driver;
